d4693a1d0bb9fbff1fd20dcc20c4b8f90d48aa58
adelowokanpelumijulius@gmail.com <Diamondchirt1995>
2017-06-05 17:18:23 +0100

    facebook/careers https://www.airbnb.com/careers clone

---
+++ dist/js/patch.js
@@ -0,0 +1,13 @@
+ var imageSources = ["dist/img/1.1.png",
+ 					 "dist/img/2.png", 
+ 					 "dist/img/3.png",
+ 					  "dist/img/4.png"]
+
+      var index = 0;
+      setInterval(function(){
+        if (index === imageSources.length) {
+          index = 0;
+        }
+        document.getElementById("changing").src = imageSources[index];
+        index++;
+      }, 10000);
